8fe536c1c68afd90e9d24a3a466501e1282ad83d
Glauco <37829079+rglauco@users.noreply.github.com>
2026-08-02 10:06:39 +0200

    bump to 1.5.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.trenirt"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.5.7"
+        versionCode = 15
+        versionName = "1.5.8"
     }
 
     buildTypes {
