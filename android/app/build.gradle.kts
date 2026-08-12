0b970304c38cdd218132ee7b44a5f1607c048f09
Glauco <37829079+rglauco@users.noreply.github.com>
2026-08-12 13:46:48 +0200

    bump to 1.5.17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "it.trenirt"
         minSdk = 26
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.5.16"
+        versionCode = 24
+        versionName = "1.5.17"
     }
 
     signingConfigs {
